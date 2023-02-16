38ed87e86d56a5cb3319ea95c0cdc1d39bb538e8
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-16 13:50:56 +0500

    task1_1

---
+++ task1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max(int agr1,int agr2,int agr3)
+{
+    int result = agr1;
+    if (agr2>result) result = agr2;
+    if(agr3>result) result = agr3;
+    return result;
+}
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 83;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 36;
+int max1 = Max (a1,b1,c1);
+int max2 = Max (a2,b2,c3);
+int max3 = Max (a3,b3,c3);
+int max = Max (max1,max2,max3);
+Console.WriteLine( max );
