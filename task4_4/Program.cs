ca3ffed92fc0618611708773d7c6b1455ea51d69
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-16 18:23:35 +0500

    task4_4

---
+++ task4_4/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArrey(int[]collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 20);
+        //index = index=1;
+        index++;
+    }
+}
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int []array = new int [20];
+FillArrey(array);
+PrintArray(array);
